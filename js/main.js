55c2c44bfffdaf6cd7b8899981d81b46ad384d91
Val@Ubuntu <mayoujord@gmail.com>
2024-11-02 17:40:17 +0100

    front-end finalization

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const navItems = document.querySelector('.nav_items');
 const openNavBtn = document.querySelector('#open_nav-btn');
 const closeNavBtn = document.querySelector('#close_nav-btn');
 
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show_sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide_sidebar-btn');
+
 // ouvre le menu déroulant de la navigation
 const openNav = () => {
     navItems.style.display = 'flex';
@@ -16,5 +20,22 @@ const closeNav = () => {
     closeNavBtn.style.display = 'none';
 }
 
+// afficher la barre latérale sur les petits appareils
+const showSidebar = () => {
+    sidebar.style.left = '0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display = 'inline-block';
+}
+
+// cacher la barre latérale sur les petits appareils
+const hideSidebar = () => {
+    sidebar.style.left = '-100%';
+    showSidebarBtn.style.display = 'inline-block';
+    hideSidebarBtn.style.display = 'none';
+}
+
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+showSidebarBtn.addEventListener('click', showSidebar)
+hideSidebarBtn.addEventListener('click', hideSidebar)
+
